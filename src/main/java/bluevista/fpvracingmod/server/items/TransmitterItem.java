fdfb522f2aca213614a9a8d458022c6410bbd985
unknown <ethan@ejohnsons.com>
2020-06-16 01:35:48 -0500

    Untracked items??

---
+++ src/main/java/bluevista/fpvracingmod/server/items/TransmitterItem.java
@@ -0,0 +1,9 @@
+package bluevista.fpvracingmod.server.items;
+
+import net.minecraft.item.Item;
+
+public class TransmitterItem extends Item {
+    public TransmitterItem(Settings settings) {
+        super(settings);
+    }
+}
