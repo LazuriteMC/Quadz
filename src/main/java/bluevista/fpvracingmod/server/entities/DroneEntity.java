cd125ff2040d14bf04fe413bf3f0fc9bdb88ae56
unknown <ethan@ejohnsons.com>
2020-07-10 14:30:05 -0500

    Cleaned up DroneEntity somewhat

--- src/main/java/bluevista/fpvracingmod/server/entities/DroneEntity.java
+++ src/main/java/bluevista/fpvracingmod/server/entities/DroneEntity.java
@@ -2,37 +2,21 @@ package bluevista.fpvracingmod.server.entities;
 
 import bluevista.fpvracingmod.server.ServerInitializer;
 import bluevista.fpvracingmod.server.items.TransmitterItem;
-import io.netty.buffer.Unpooled;
-import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MovementType;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.ProjectileDamageSource;
-import net.minecraft.entity.data.DataTracker;
-import net.minecraft.entity.data.TrackedData;
-import net.minecraft.entity.data.TrackedDataHandler;
-import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Packet;
-import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.tag.BlockTags;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.crash.CrashException;
-import net.minecraft.util.crash.CrashReport;
-import net.minecraft.util.crash.CrashReportSection;
 import net.minecraft.util.math.*;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 
 import java.util.List;
@@ -43,6 +27,8 @@ public class DroneEntity extends Entity {
 
 	private int band;
 	private int channel;
+	private int cameraAngle;
+
 	private Quaternion orientation;
 	private float throttle;
 
@@ -55,29 +41,17 @@ public class DroneEntity extends Entity {
 
 		this.orientation = new Quaternion(0, 1, 0, 0);
 
-		Random random = new Random(); // testing only
 		band = random.nextInt(6) + 1; // 1 - 6
 		channel = random.nextInt(8) + 1; // 1 - 8
-
-		// TODO nbt tags - channel, camera_angle, etc.
+		cameraAngle = 20;
 	}
 
 	@Override
 	public void tick() {
 		super.tick();
-//		System.out.println("Is Server?: " + isLogicalSideForUpdatingMovement());
-//		if(isLogicalSideForUpdatingMovement()) {
-			this.setVelocity(0.01*getThrottle(), 0, 0);
-//			this.updateTrackedPosition(getX(), getY(), getZ());
-//			this.addVelocity(1000, 0, 0);
-			this.move(MovementType.SELF, this.getVelocity());
-//		}
 
-//		System.out.println("X: " + this.getX());
-//		System.out.println("Y: " + this.getY());
-//		System.out.println("Z: " + this.getZ());
-//		System.out.println();
-//		this.setPos(getX() + getVelocity().getX(), getY() + getVelocity().getY(), getZ() + getVelocity().getZ());
+		this.setVelocity(0.01*getThrottle(), 0, 0);
+		this.move(MovementType.SELF, this.getVelocity());
 
 //		if(RenderHandler.isPlayerViewingDrone()) {
 //			Vector3f d = QuaternionHelper.rotationMatrixToVector(QuaternionHelper.quatToMatrix(getOrientation()));
@@ -94,10 +68,6 @@ public class DroneEntity extends Entity {
 //					if(getDistanceSq(player) >= 2500) {
 //						player.connection.setPlayerLocation((float) Math.round(getPositionVec().x), 50, (float) Math.round(getPositionVec().z), player.rotationYaw, player.rotationPitch);
 //						if (!player.abilities.isCreativeMode) player.abilities.allowFlying = true;
-//					}
-//				}
-//			}
-//		}
 
 	}
 
@@ -105,12 +75,14 @@ public class DroneEntity extends Entity {
 	protected void readCustomDataFromTag(CompoundTag tag) {
 		band = tag.getInt("band");
 		channel = tag.getInt("channel");
+		cameraAngle = tag.getInt("camera_angle");
 	}
 
 	@Override
 	protected void writeCustomDataToTag(CompoundTag tag) {
 		tag.putInt("band", band);
 		tag.putInt("channel", channel);
+		tag.putInt("camera_angle", cameraAngle);
 	}
 
 	@Override
@@ -122,9 +94,15 @@ public class DroneEntity extends Entity {
 		World world = entity.getEntityWorld();
 		List<DroneEntity> drones = world.getEntities(
 				DroneEntity.class,
-				new Box(entity.getPos().getX() - 100, entity.getPos().getY() - 100, entity.getPos().getZ() - 100, entity.getPos().getX() + 100, entity.getPos().getY() + 100, entity.getPos().getZ() + 100),
+				new Box(entity.getPos().getX() - 100,
+						entity.getPos().getY() - 100,
+						entity.getPos().getZ() - 100,
+						entity.getPos().getX() + 100,
+						entity.getPos().getY() + 100,
+						entity.getPos().getZ() + 100),
 				null
 		);
+
 		if (drones.size() > 0) return drones.get(0);
 		else return null;
 	}
@@ -133,7 +111,12 @@ public class DroneEntity extends Entity {
 		World world = entity.getEntityWorld();
 		List<DroneEntity> drones = world.getEntities(
 				DroneEntity.class,
-				new Box(entity.getPos().getX() - 100, entity.getPos().getY() - 100, entity.getPos().getZ() - 100, entity.getPos().getX() + 100, entity.getPos().getY() + 100, entity.getPos().getZ() + 100),
+				new Box(entity.getPos().getX() - 100,
+						entity.getPos().getY() - 100,
+						entity.getPos().getZ() - 100,
+						entity.getPos().getX() + 100,
+						entity.getPos().getY() + 100,
+						entity.getPos().getZ() + 100),
 				null
 		);
 
@@ -150,11 +133,12 @@ public class DroneEntity extends Entity {
 	@Override
 	public boolean damage(DamageSource source, float amount) {
 		if (source instanceof ProjectileDamageSource || source.getAttacker() instanceof PlayerEntity) {
-//			if (this.world.getGameRules().getBoolean(GameRules.DO_ENTITY_DROPS))
+			if (this.world.getGameRules().getBoolean(GameRules.DO_ENTITY_DROPS))
 			this.dropItem(ServerInitializer.DRONE_SPAWNER_ITEM.asItem());
 			this.remove();
 			return true;
 		}
+
 		return false;
 	}
 
@@ -162,14 +146,11 @@ public class DroneEntity extends Entity {
 		if(!player.world.isClient()) {
 			if (player.inventory.getMainHandStack().getItem() instanceof TransmitterItem) {
 				CompoundTag subTag = player.inventory.getMainHandStack().getOrCreateSubTag("bind");
-//				if(subTag.getUuid("bind").equals(this.getUuid())) {
-//					player.sendMessage(new TranslatableText("Already bound"), false);
-//				} else {
-					subTag.putUuid("bind", this.getUuid());
-					player.sendMessage(new TranslatableText("Transmitter bound"), false);
-//				}
+				subTag.putUuid("bind", this.getUuid());
+				player.sendMessage(new TranslatableText("Transmitter bound"), false);
 			}
 		}
+
 		return ActionResult.SUCCESS;
 	}
 
@@ -199,6 +180,7 @@ public class DroneEntity extends Entity {
 
 	@Override
 	protected void initDataTracker() {
+
 	}
 
 	@Override
@@ -211,11 +193,11 @@ public class DroneEntity extends Entity {
 		return true;
 	}
 
-//	@Override
-//	public Box getCollisionBox() {
-//		return super.getBoundingBox();
-//	}
-//
+	@Override
+	public Box getCollisionBox() {
+		return super.getBoundingBox();
+	}
+
 //	@Override
 //	public Box getHardCollisionBox(Entity collidingEntity) {
 //		return collidingEntity.isPushable() ? collidingEntity.getBoundingBox() : null;
