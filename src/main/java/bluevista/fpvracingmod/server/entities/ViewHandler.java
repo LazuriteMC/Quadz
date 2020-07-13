25af56a0e2d6fe98c8c51f9511584d7f9f17f839
unknown <ethan@ejohnsons.com>
2020-07-13 17:27:02 -0500

    Put ViewHandler .1 blocks above the ground

--- src/main/java/bluevista/fpvracingmod/server/entities/ViewHandler.java
+++ src/main/java/bluevista/fpvracingmod/server/entities/ViewHandler.java
@@ -37,7 +37,7 @@ public class ViewHandler extends Entity {
     public void clientTick(float delta) {
         if(target != null) {
             float deltaPosX = (float) (this.prevPosX + (this.target.getPos().x - this.prevPosX) * delta);
-            float deltaPosY = (float) (this.prevPosY + (this.target.getPos().y-1 - this.prevPosY) * delta);
+            float deltaPosY = (float) (this.prevPosY + (this.target.getPos().y-1.4 - this.prevPosY) * delta);
             float deltaPosZ = (float) (this.prevPosZ + (this.target.getPos().z - this.prevPosZ) * delta);
 
             this.resetPosition(deltaPosX, deltaPosY, deltaPosZ);
