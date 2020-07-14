1ef34e263e31daca642b71aef2da1be39b0128eb
unknown <ethan@ejohnsons.com>
2020-07-14 00:37:10 -0500

    Fixed missing hand mystery

--- src/main/java/bluevista/fpvracingmod/client/ClientTick.java
+++ src/main/java/bluevista/fpvracingmod/client/ClientTick.java
@@ -41,6 +41,8 @@ public class ClientTick {
 
             if(!isWearingGoggles(mc.player) && mc.getCameraEntity() instanceof ViewHandler)
                 resetView(mc);
+        } else {
+            resetView(mc);
         }
     }
 
