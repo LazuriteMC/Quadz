741b39e11e312c7b980572dfd895b24958f9ae61
unknown <ethan@ejohnsons.com>
2020-09-20 17:43:24 -0500

    Documented Client and Server PlayerInteractionManager Mixins

--- src/main/java/bluevista/fpvracingmod/mixin/ClientPlayerInteractionManagerMixin.java
+++ src/main/java/bluevista/fpvracingmod/mixin/ClientPlayerInteractionManagerMixin.java
@@ -1,14 +1,29 @@
 package bluevista.fpvracingmod.mixin;
 
 import bluevista.fpvracingmod.server.items.DroneSpawnerItem;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+/**
+ * Mixin class for {@link ClientPlayerInteractionManager}.
+ * @author Ethan Johnson
+ */
 @Mixin(ClientPlayerInteractionManager.class)
 public class ClientPlayerInteractionManagerMixin {
+    /**
+     * Redirects the {@link ItemStack#setCount(int)} method call in
+     * {@link ClientPlayerInteractionManager#interactBlock(ClientPlayerEntity, ClientWorld, Hand, BlockHitResult)}
+     * in order to allow the count to reach zero in creative mode.
+     * @param itemStack the {@link ItemStack} on which {@link ItemStack#setCount(int)} was called on
+     * @param count the count to set the {@link ItemStack} to
+     */
     @Redirect(
             method = "interactBlock",
             at = @At(
@@ -24,7 +39,6 @@ public class ClientPlayerInteractionManagerMixin {
         }
 
         if(itemStack.getItem() instanceof DroneSpawnerItem) {
-            System.out.println("SETTINGS: " + realCount);
             itemStack.setCount(realCount);
         } else {
             itemStack.setCount(count);
--- src/main/java/bluevista/fpvracingmod/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/bluevista/fpvracingmod/mixin/ServerPlayerInteractionManagerMixin.java
@@ -2,13 +2,28 @@ package bluevista.fpvracingmod.mixin;
 
 import bluevista.fpvracingmod.server.items.DroneSpawnerItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+/**
+ * Mixin class for {@link ServerPlayerInteractionManager}.
+ * @author Ethan Johnson
+ */
 @Mixin(ServerPlayerInteractionManager.class)
 public class ServerPlayerInteractionManagerMixin {
+    /**
+     * Redirects the {@link ItemStack#setCount(int)} method call in
+     * {@link ServerPlayerInteractionManager#interactBlock(ServerPlayerEntity, World, ItemStack, Hand, BlockHitResult)}
+     * in order to allow the count to reach zero in creative mode.
+     * @param itemStack the {@link ItemStack} on which {@link ItemStack#setCount(int)} was called on
+     * @param count the count to set the {@link ItemStack} to
+     */
     @Redirect(
             method = "interactBlock",
             at = @At(
@@ -24,7 +39,6 @@ public class ServerPlayerInteractionManagerMixin {
         }
 
         if(itemStack.getItem() instanceof DroneSpawnerItem) {
-            System.out.println("SETTINGS: " + realCount);
             itemStack.setCount(realCount);
         } else {
             itemStack.setCount(count);
