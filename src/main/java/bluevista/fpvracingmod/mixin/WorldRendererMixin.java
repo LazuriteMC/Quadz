71a34c55abf922b99f2bf8807d8f4dd5f96e06ed
unknown <ethan@ejohnsons.com>
2020-09-19 20:25:12 -0500

    Made player render when in drone

---
+++ src/main/java/bluevista/fpvracingmod/mixin/WorldRendererMixin.java
@@ -0,0 +1,34 @@
+package bluevista.fpvracingmod.mixin;
+
+import bluevista.fpvracingmod.client.ClientInitializer;
+import bluevista.fpvracingmod.server.entities.DroneEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(WorldRenderer.class)
+public class WorldRendererMixin {
+    @Shadow @Final MinecraftClient client;
+
+    @Redirect(
+            method = "render",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/render/Camera;getFocusedEntity()Lnet/minecraft/entity/Entity;",
+                    ordinal = 3
+            )
+    )
+    public Entity getFocusedEntity(Camera camera) {
+        if(camera.getFocusedEntity() instanceof DroneEntity) {
+            return client.player;
+        }
+
+        return camera.getFocusedEntity();
+    }
+}
