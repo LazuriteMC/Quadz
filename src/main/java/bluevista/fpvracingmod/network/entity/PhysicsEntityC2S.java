02937ad806758e8d6a5a7e68d04703a9dd223b21
unknown <ethan@ejohnsons.com>
2020-09-22 00:54:52 -0500

    Added RotateAndMoveRelativeMmixin

--- src/main/java/bluevista/fpvracingmod/network/entity/PhysicsEntityC2S.java
+++ src/main/java/bluevista/fpvracingmod/network/entity/PhysicsEntityC2S.java
@@ -36,12 +36,10 @@ public class PhysicsEntityC2S {
                 physics = (PhysicsEntity) player.world.getEntityById(id);
 
             if (physics != null) {
-                physics.yaw = yaw;
-                physics.pitch = pitch;
-
                 physics.setOrientation(orientation);
                 physics.setRigidBodyPos(position);
-                physics.resetPosition(position.x, position.y, position.z);
+                physics.updatePosition(position.x, position.y, position.z);
+                physics.refreshPositionAndAngles(position.x, position.y, position.z, yaw, pitch);
                 physics.getRigidBody().setLinearVelocity(linearVel);
                 physics.getRigidBody().setAngularVelocity(angularVel);
             }
