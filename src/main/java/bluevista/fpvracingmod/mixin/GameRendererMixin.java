8c9b16d8339ed7c3a5e9d8c38068cd003a7c367c
unknown <ethan@ejohnsons.com>
2020-09-23 18:44:54 -0500

    Documented GameRendererMixin

--- src/main/java/bluevista/fpvracingmod/mixin/GameRendererMixin.java
+++ src/main/java/bluevista/fpvracingmod/mixin/GameRendererMixin.java
@@ -20,6 +20,41 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class GameRendererMixin {
 	@Shadow MinecraftClient client;
 
+	/**
+	 * Mainly for calling the {@link RenderTick#tick(MinecraftClient, MatrixStack, float)} and
+	 * {@link InputTick#tick()} methods. They must run every frame rather than every tick.
+	 * @param tickDelta minecraft tick delta
+	 * @param limitTime
+	 * @param matrix the matrix stack (used in {@link RenderTick#tick(MinecraftClient, MatrixStack, float)} to rotate the screen)
+	 * @param info required by every mixin injection
+	 */
+	@Inject(at = @At("HEAD"), method = "renderWorld")
+	public void renderWorld(float tickDelta, long limitTime, MatrixStack matrix, CallbackInfo info) {
+		RenderTick.tick(client, matrix, tickDelta);
+		InputTick.tick();
+	}
+
+	/**
+	 * This mixin modifies the renderHand method so that when the player
+	 * is flying a drone, the hand is no longer renderer.
+	 * @param matrices the matrix stack
+	 * @param camera the camera object
+	 * @param tickDelta minecraft tick delta
+	 * @param info required by every mixin injection
+	 */
+	@Inject(at = @At("HEAD"), method = "renderHand", cancellable = true)
+	public void renderHand(MatrixStack matrices, Camera camera, float tickDelta, CallbackInfo info) {
+		if (ClientInitializer.isInGoggles(client)) info.cancel();
+	}
+
+	/**
+	 * This mixin redirects the {@link MatrixStack#multiply(Quaternion)} method called in
+	 * {@link GameRenderer#renderWorld(float, long, MatrixStack)}. If the player is flying
+	 * a drone, this method will lock the screens yaw to 0 degrees. Otherwise, it will pass
+	 * through like normal.
+	 * @param stack the matrix stack
+	 * @param quat the quaternion to rotate by
+	 */
 	@Redirect(
 			method = "renderWorld(FJLnet/minecraft/client/util/math/MatrixStack;)V",
 			at = @At(
@@ -30,12 +65,20 @@ public class GameRendererMixin {
 	)
 	public void yaw(MatrixStack stack, Quaternion quat) {
 		if (ClientInitializer.isInGoggles(client)) {
-			stack.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(180));
+			stack.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(0));
 		} else {
 			stack.multiply(quat);
 		}
 	}
 
+	/**
+	 * This mixin redirects the {@link MatrixStack#multiply(Quaternion)} method called in
+	 * {@link GameRenderer#renderWorld(float, long, MatrixStack)}. If the player is flying
+	 * a drone, this method will lock the screens pitch to 0 degrees. Otherwise, it will pass
+	 * through like normal.
+	 * @param stack the matrix stack
+	 * @param quat the quaternion to rotate by
+	 */
 	@Redirect(
 			method = "renderWorld(FJLnet/minecraft/client/util/math/MatrixStack;)V",
 			at = @At(
@@ -50,24 +93,5 @@ public class GameRendererMixin {
 		} else {
 			stack.multiply(quat);
 		}
-	}
-
-	/*
-	 * Call the RenderTick#tick and InputTick#tick methods within renderWorld.
-	 * The idea is that they will run every frame rather than every game tick.
-	 */
-	@Inject(at = @At("HEAD"), method = "renderWorld")
-	public void renderWorld(float tickDelta, long limitTime, MatrixStack matrix, CallbackInfo info) {
-		RenderTick.tick(client, matrix, tickDelta);
-		InputTick.tick();
-	}
-
-	/*
-	 * Cancel the method which renders the player's hand if the
-	 * player is viewing a drone through goggles.
-	 */
-	@Inject(at = @At("HEAD"), method = "renderHand", cancellable = true)
-	public void renderHand(MatrixStack matrices, Camera camera, float tickDelta, CallbackInfo info) {
-		if (ClientInitializer.isInGoggles(client)) info.cancel();
 	}
 }
