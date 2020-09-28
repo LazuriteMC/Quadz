b5694f498edf6837d77c3bd0f9b838fe55e3e1dc
unknown <ethan@ejohnsons.com>
2020-09-27 22:37:12 -0500

    Documented EMPC2S class

--- src/main/java/bluevista/fpvracing/network/keybinds/EMPC2S.java
+++ src/main/java/bluevista/fpvracing/network/keybinds/EMPC2S.java
@@ -15,9 +15,19 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 
+/**
+ * This packet is sent whenever the player types the EMP command or presses
+ * the EMP keybinding. There really isn't any information to include since
+ * receiving this packet only performs one operation involving no data.
+ */
 public class EMPC2S {
     public static final Identifier PACKET_ID = new Identifier(ServerInitializer.MODID, "emp_c2s");
 
+    /**
+     * Accepts the packet. No information is really sent besides the current {@link PlayerEntity}.
+     * @param context the packet context
+     * @param buf an empty buffer
+     */
     public static void accept(PacketContext context, PacketByteBuf buf) {
         PlayerEntity playerEntity = context.getPlayer();
 
@@ -38,10 +48,17 @@ public class EMPC2S {
         });
     }
 
+    /**
+     * Sends the packet to the server. No information is passed in or included
+     * within the {@link PacketByteBuf}.
+     */
     public static void send() {
         ClientSidePacketRegistry.INSTANCE.sendToServer(PACKET_ID, new PacketByteBuf(Unpooled.buffer()));
     }
 
+    /**
+     * Registers the packet in {@link ServerInitializer}.
+     */
     public static void register() {
         ServerSidePacketRegistry.INSTANCE.register(PACKET_ID, EMPC2S::accept);
     }
