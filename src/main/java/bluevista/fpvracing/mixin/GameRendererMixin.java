f9980e784868372c9a24c5b52e09bd207981b315
Ethan Johnson <ethan@ejohnsons.com>
2020-11-06 13:07:37 -0600

    Wrote HeldItemRendererMixin

--- src/main/java/bluevista/fpvracing/mixin/GameRendererMixin.java
+++ src/main/java/bluevista/fpvracing/mixin/GameRendererMixin.java
@@ -4,9 +4,9 @@ import bluevista.fpvracing.client.ClientInitializer;
 import bluevista.fpvracing.client.RenderTick;
 import bluevista.fpvracing.client.input.InputTick;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,7 +21,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
  */
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
-	@Shadow MinecraftClient client;
+	@Shadow @Final private MinecraftClient client;
+//	@Shadow private boolean renderHand;
 
 	/**
 	 * Mainly for calling the {@link RenderTick#tick(MinecraftClient, MatrixStack, float)} and
@@ -35,18 +36,6 @@ public class GameRendererMixin {
 	public void renderWorld(float tickDelta, long limitTime, MatrixStack matrix, CallbackInfo info) {
 		RenderTick.tick(client, matrix, tickDelta);
 		InputTick.tick();
-	}
-
-	/**
-	 * This mixin modifies the renderHand method so that when the player
-	 * is flying a drone, the hand is no longer renderer.
-	 * @param matrices the matrix stack
-	 * @param camera the camera object
-	 * @param tickDelta minecraft tick delta
-	 * @param info required by every mixin injection
-	 */
-	@Inject(at = @At("HEAD"), method = "renderHand", cancellable = true)
-	public void renderHand(MatrixStack matrices, Camera camera, float tickDelta, CallbackInfo info) {
-		if (ClientInitializer.isInGoggles(client)) info.cancel();
+//		renderHand = !ClientInitializer.isInGoggles(client);
 	}
 }
---
+++ src/main/java/bluevista/fpvracing/mixin/HeldItemRendererMixin.java
@@ -0,0 +1,26 @@
+package bluevista.fpvracing.mixin;
+
+import bluevista.fpvracing.server.entities.DroneEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.item.HeldItemRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(HeldItemRenderer.class)
+public class HeldItemRendererMixin {
+    @Shadow @Final private MinecraftClient client;
+
+    @Inject(at = @At("HEAD"), method = "renderItem(FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;Lnet/minecraft/client/network/ClientPlayerEntity;I)V", cancellable = true)
+    public void renderItem(float tickDelta, MatrixStack matrices, VertexConsumerProvider.Immediate vertexConsumers, ClientPlayerEntity player, int light, CallbackInfo info) {
+        if (client.getCameraEntity() instanceof DroneEntity) {
+            info.cancel();
+        }
+    }
+}
