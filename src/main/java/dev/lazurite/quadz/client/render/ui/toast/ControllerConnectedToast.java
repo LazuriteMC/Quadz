c255be274ae31791f80740363f11491156d379f6
Patrick Hofmann <patrickhofmanndesigns@gmail.com>
2022-01-03 17:57:06 -0600

    Fixed toasts

--- src/main/java/dev/lazurite/quadz/client/render/ui/toast/ControllerConnectedToast.java
+++ src/main/java/dev/lazurite/quadz/client/render/ui/toast/ControllerConnectedToast.java
@@ -1,10 +1,12 @@
 package dev.lazurite.quadz.client.render.ui.toast;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import dev.lazurite.quadz.Quadz;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.toasts.Toast;
 import net.minecraft.client.gui.components.toasts.ToastComponent;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.item.ItemStack;
@@ -25,15 +27,14 @@ public class ControllerConnectedToast implements Toast {
 
     @Override
     public Visibility render(PoseStack poseStack, ToastComponent toastComponent, long startTime) {
-        toastComponent.getMinecraft().getTextureManager().bindForSetup(TEXTURE);
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         toastComponent.blit(poseStack, 0, 0, 0, 0, width(), height());
+
         toastComponent.getMinecraft().font.draw(poseStack, message, 30, 7, -1);
         toastComponent.getMinecraft().font.draw(poseStack, new TextComponent(controllerName), 30, 18, -1);
-
-        poseStack.pushPose();
-        poseStack.scale(1.5f, 1.5f, 1.0f);
-        toastComponent.getMinecraft().getItemRenderer().renderAndDecorateFakeItem(new ItemStack(Quadz.TRANSMITTER_ITEM), 3, 3);
-        poseStack.popPose();
+        toastComponent.getMinecraft().getItemRenderer().renderAndDecorateFakeItem(new ItemStack(Quadz.TRANSMITTER_ITEM), 8, 8);
 
         return startTime >= 5000L ? Toast.Visibility.HIDE : Toast.Visibility.SHOW;
     }
--- src/main/java/dev/lazurite/quadz/client/render/ui/toast/ControllerNotFoundToast.java
+++ src/main/java/dev/lazurite/quadz/client/render/ui/toast/ControllerNotFoundToast.java
@@ -1,10 +1,12 @@
 package dev.lazurite.quadz.client.render.ui.toast;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import dev.lazurite.quadz.Quadz;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.toasts.Toast;
 import net.minecraft.client.gui.components.toasts.ToastComponent;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.ItemStack;
 
@@ -15,14 +17,13 @@ public class ControllerNotFoundToast implements Toast {
 
     @Override
     public Visibility render(PoseStack poseStack, ToastComponent toastComponent, long startTime) {
-        toastComponent.getMinecraft().getTextureManager().bindForSetup(TEXTURE);
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         toastComponent.blit(poseStack, 0, 0, 0, 0, width(), height());
-        toastComponent.getMinecraft().font.draw(poseStack, new TranslatableComponent("toast.quadz.controller.notfound"), 30, 12, -1);
 
-        poseStack.pushPose();
-        poseStack.scale(1.5f, 1.5f, 1.0f);
-        toastComponent.getMinecraft().getItemRenderer().renderAndDecorateFakeItem(new ItemStack(Quadz.TRANSMITTER_ITEM), 3, 3);
-        poseStack.popPose();
+        toastComponent.getMinecraft().font.draw(poseStack, new TranslatableComponent("toast.quadz.controller.notfound"), 30, 12, -1);
+        toastComponent.getMinecraft().getItemRenderer().renderAndDecorateFakeItem(new ItemStack(Quadz.TRANSMITTER_ITEM), 8, 8);
 
         this.visible = startTime < visibilityTime;
         return startTime >= visibilityTime ? Visibility.HIDE : Visibility.SHOW;
