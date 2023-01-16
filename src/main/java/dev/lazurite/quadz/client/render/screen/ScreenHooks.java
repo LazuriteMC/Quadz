11b62a3b1ad0d1079982f604b3eaefb7e2219e6b
ethanejohnsons <ethan@ejohnsons.com>
2023-01-16 15:34:04 -0600

    Added quadz config button texture

--- src/main/java/dev/lazurite/quadz/client/render/screen/ScreenHooks.java
+++ src/main/java/dev/lazurite/quadz/client/render/screen/ScreenHooks.java
@@ -1,24 +1,27 @@
 package dev.lazurite.quadz.client.render.screen;
 
+import dev.lazurite.quadz.Quadz;
 import dev.lazurite.quadz.client.render.screen.ConfigScreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.GridWidget;
+import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 
 public class ScreenHooks {
 
-    public static void addQuadzButton(PauseScreen screen, GridWidget gridWidget, GridWidget.RowHelper rowHelper) {
-        screen.addRenderableWidget(Button.builder(
-                Component.literal("Q"),
-                button -> {
-                    Minecraft.getInstance().setScreen(new ConfigScreen());
-                }
-        )
-        .size(20, 20)
-        .pos(screen.disconnectButton.getX() + screen.disconnectButton.getWidth() + 5, screen.disconnectButton.getY())
-        .build());
+    public static void addQuadzButton(PauseScreen screen) {
+        screen.addRenderableWidget(new ImageButton(
+                screen.disconnectButton.getX() + screen.disconnectButton.getWidth() + 5,
+                screen.disconnectButton.getY(),
+                20, 20, 0, 0, 20,
+                new ResourceLocation(Quadz.MODID, "textures/gui/quadz.png"),
+                32, 64,
+                button -> Minecraft.getInstance().setScreen(new ConfigScreen()),
+                Component.translatable("quadz.config.title")
+        ));
     }
 
 }
